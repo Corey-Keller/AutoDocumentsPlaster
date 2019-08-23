9745cb500f57bd89a36644505c752c77e6d9c3f5
Corey Keller <corey@keller.net>
2019-08-23 16:52:32 -0500

    Add stub functions

---
+++ template/ModuleName/Public/Verb-Noun.ps1
@@ -0,0 +1,33 @@
+function Verb-Noun
+{
+  <#
+  .SYNOPSIS
+    Short Description
+  .DESCRIPTION
+    Long Description
+  .PARAMETER ParameterName
+    Description of what the parameter does
+  .EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+  .INPUTS
+    Inputs (if any)
+  .OUTPUTS
+    Output (if any)
+  .NOTES
+    General notes
+  .LINK
+    https://<%=$PLASTER_PARAM_GitHubDomain%>/<%=$PLASTER_PARAM_GitHubUserName%>/<%=$PLASTER_PARAM_ModuleName%>/blob/master/<%=$PLASTER_PARAM_ModuleName%>/Public/Verb-Noun.ps1
+    [CmdletBinding(DefaultParameterSetName='DefaultParamSet', 
+                SupportsShouldProcess=$true, 
+                PositionalBinding=$false,
+                HelpUri = "https://<%=$PLASTER_PARAM_GitHubDomain%>/<%=$PLASTER_PARAM_GitHubUserName%>/<%=$PLASTER_PARAM_ModuleName%>/blob/master/<%=$PLASTER_PARAM_ModuleName%>/Public/Verb-Noun.ps1",
+                ConfirmImpact='Medium')]
+    Param
+    (
+
+    )
+
+
+
+}
